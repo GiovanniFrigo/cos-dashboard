a0e7d3d32590710e1912324bb8289d6a4f0b1687
fedriz <9001053+fedriz@users.noreply.github.com>
2020-01-23 16:03:49 +0100

    remove call to api

--- project/scheduler.py
+++ project/scheduler.py
@@ -2,13 +2,18 @@ import time
 
 import requests
 import schedule
+from django.http import HttpRequest
+
+from app.views import create_registry
 
 
 def job():
-    try:
-        requests.get("http://localhost:8000/e0566b3ccc1c6eec6dace94cc3f9b776/")
-    except requests.exceptions.RequestException as e:
-        print(e)
+    fake_request = HttpRequest()
+    create_registry(fake_request)
+    # try:
+    #     requests.get("http://localhost:8000/e0566b3ccc1c6eec6dace94cc3f9b776/")
+    # except requests.exceptions.RequestException as e:
+    #     print(e)
 
 
 schedule.every(10).seconds.do(job)
