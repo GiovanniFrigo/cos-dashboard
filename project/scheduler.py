4beb38b265cf5462096b1bda69bdc1c98d6dc988
fedriz <9001053+fedriz@users.noreply.github.com>
2020-01-23 16:16:14 +0100

    retry with request

--- project/scheduler.py
+++ project/scheduler.py
@@ -4,16 +4,16 @@ import requests
 import schedule
 from django.http import HttpRequest
 
-from app.views_normal import create_registry
+# from app.views_normal import create_registry
 
 
 def job():
-    fake_request = HttpRequest()
-    create_registry(fake_request)
-    # try:
-    #     requests.get("http://localhost:8000/e0566b3ccc1c6eec6dace94cc3f9b776/")
-    # except requests.exceptions.RequestException as e:
-    #     print(e)
+    # fake_request = HttpRequest()
+    # create_registry(fake_request)
+    try:
+        requests.get("http://localhost:8000/e0566b3ccc1c6eec6dace94cc3f9b776/")
+    except requests.exceptions.RequestException as e:
+        print(e)
 
 
 schedule.every(10).seconds.do(job)
